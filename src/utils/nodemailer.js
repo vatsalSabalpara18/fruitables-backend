06f4adf09ae8797106648ba98c152973b708f6bf
vatsalSabalpara18 <vatsalsabalpara18@gmail.com>
2025-04-26 09:40:01 +0530

    init setup

--- src/utils/nodemailer.js
+++ src/utils/nodemailer.js
@@ -1,39 +1,55 @@
 
-const nodemailer = require('nodemailer');
+// const nodemailer = require('nodemailer');
+const { Resend } = require('resend');
 
+// const sendMail = async (email, title, message) => {
+//     try {
+
+//         var transporter = await nodemailer.createTransport({
+//             service: 'gmail',
+//             auth: {
+//                 user: 'vatsalsabalpara18@gmail.com',
+//                 pass: 'jxlx eamn fiba ckty'
+//             },
+
+//             tls: {
+//                 rejectUnauthorized: false
+//             }
+
+//         });
+
+//         var mailOptions = {
+//             from: 'vatsalsabalpara18@gmail.com',
+//             to: email,
+//             subject: title,
+//             text: message
+//         };
+        
+//         await transporter.sendMail(mailOptions, function (error, info) {
+//             if (error) {
+//                 console.log(error);
+//             } else {
+//                 console.log('Email sent: ' + info.response);
+//             }
+//         });
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
 
 const sendMail = async (email, title, message) => {
-    try {
+    try {        
+        const resend = new Resend(process.env.RESEND_API_KEY);
 
-        var transporter = await nodemailer.createTransport({
-            service: 'gmail',
-            auth: {
-                user: 'vatsalsabalpara18@gmail.com',
-                pass: 'jxlx eamn fiba ckty'
-            },
-
-            tls: {
-                rejectUnauthorized: false
-            }
-
-        });
-
-        var mailOptions = {
-            from: 'vatsalsabalpara18@gmail.com',
+        await resend.emails.send({
+            from: 'onboarding@resend.dev',
             to: email,
             subject: title,
-            text: message
-        };
-        
-        await transporter.sendMail(mailOptions, function (error, info) {
-            if (error) {
-                console.log(error);
-            } else {
-                console.log('Email sent: ' + info.response);
-            }
+            html: `<p>${message}</p>`
         });
+        return true;
     } catch (error) {
-        console.log(error);
+        throw new Error("Error in Sending Email", error.message);
     }
 }
 
